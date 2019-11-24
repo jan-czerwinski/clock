a2ee8bc1335994b686cdd9009fd11de7fab3829a
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2019-11-24 23:36:44 +0100

    Fix start shell script

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd $SCRIPTPATH
 cd clock-ble/src
-nohup node main.js &> ../../logs/clock-ble.log & echo $! > ../../logs/pids
+nohup node main.js > ../../logs/clock-ble.log 2>&1 & echo $! > ../../logs/pids
 
 cd $SCRIPTPATH
 cd clock-logic
-nohup python3 main.py &> ../logs/clock-logic.log & echo $! >> ../logs/pids
+nohup python3 main.py > ../logs/clock-logic.log 2>&1 & echo $! >> ../logs/pids
 
 echo Clock started, output in $SCRIPTPATH/logs
