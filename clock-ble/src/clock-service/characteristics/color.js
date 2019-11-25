e58a5f8d1cf9010997fef46a7cef5cf318723131
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2019-11-25 19:29:54 +0100

    Add working read to characteristics

--- clock-ble/src/clock-service/characteristics/color.js
+++ clock-ble/src/clock-service/characteristics/color.js
@@ -2,7 +2,7 @@ const bleno = require("bleno");
 
 const parseColor = data => {
   data = data.readUInt32LE(0);
-  color = {
+  const color = {
     r: Math.floor(data / 1000000),
     g: Math.floor((data % 1000000) / 1000),
     b: data % 1000
@@ -10,18 +10,23 @@ const parseColor = data => {
   return color;
 };
 
+const compressColor = color => {
+  const buf = Buffer.alloc(4);
+  const { r, g, b } = color;
+  buf.writeUInt32LE(parseInt(`${r}${g}${b}`));
+  return buf;
+};
+
 class Color extends bleno.Characteristic {
   constructor(Clock) {
     super({
       uuid: "3cefb003-82c1-4929-8021-6d793424227c",
-      properties: ["read", "write"],
-      value: null
+      properties: ["read", "write"]
     });
     this.Clock = Clock;
-    this._value = null;
   }
   onReadRequest(offset, callback) {
-    callback(this.RESULT_SUCCESS, this._value);
+    callback(this.RESULT_SUCCESS, compressColor(this.Clock.data.color));
   }
   onWriteRequest(data, offset, withoutResponse, callback) {
     if (offset) {
@@ -29,7 +34,6 @@ class Color extends bleno.Characteristic {
     } else if (data.length !== 4) {
       callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
     } else {
-      this._value = data;
       this.Clock.data.color = parseColor(data);
       this.Clock.dataChanged();
       callback(this.RESULT_SUCCESS);
